d4e635629aa60cb4b3948d8bf46aaae1277feef9
namgay tenzin <namgay.tenzin@dcpl.bt>
2022-07-16 15:28:56 +0600

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
-      expose: ['Authorization']
-    end
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
+    expose: ['Authorization']
   end
 end

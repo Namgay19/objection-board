fa9544439d311ea654a96dbc5494436c87546312
namgay tenzin <namgay.tenzin@dcpl.bt>
2022-10-05 14:10:09 +0600

    change 3 to 4 for work experience

--- db/migrate/20220930092205_add_viewed_at.rb
+++ db/migrate/20220930092205_add_viewed_at.rb
@@ -3,6 +3,6 @@ class AddViewedAt < ActiveRecord::Migration[7.0]
     add_column :books, :viewed_at, :datetime, index: true
     add_column :audios, :viewed_at, :datetime, index: true
     add_column :videos, :viewed_at, :datetime, index: true
-    add_column :stories, :viewed_at, :datetime, index: true 
+    add_column :stories, :viewed_at, :datetime, index: true
   end
 end

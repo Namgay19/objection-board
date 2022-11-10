7fe9e9cf304724ffd6868ca1850500f5fd5f1e5c
namgay tenzin <namgay.tenzin@dcpl.bt>
2022-11-10 15:25:19 +0600

    make it nullable

--- app/graphql/types/financial_meta_type.rb
+++ app/graphql/types/financial_meta_type.rb
@@ -2,10 +2,10 @@
 
 module Types
   class FinancialMetaType < Types::BaseObject
-    field :daily_target, Integer, null: false
-    field :weekly_target, Integer, null: false
-    field :monthly_target, Integer, null: false
-    field :yearly_target, Integer, null: false
-    field :client_annual_revenue, Integer, null: false
+    field :daily_target, Integer, null: true
+    field :weekly_target, Integer, null: true
+    field :monthly_target, Integer, null: true
+    field :yearly_target, Integer, null: true
+    field :client_annual_revenue, Integer, null: true
   end
 end
